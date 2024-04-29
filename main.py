523da54417259462e8f646601ccfcca6c596e4b4
Honza66 <luiggi@seznam.cz>
2024-04-29 15:02:23 +0200

    vytvoření funkce translate_text

--- main.py
+++ main.py
@@ -8,6 +8,15 @@ root.title('Konvertot textu na řeč')
 root.geometry('300x300')
 root.resizable(False, False)
 
+def translate_text(entry, language, audio_file):
+    try:
+        text_to_audio = entry
+        output = gTTS(text=text_to_audio, lang= language,  slow=False)
+        output.save(f'{audio_file}.mp3')
+        os.system(f'{audio_file}.mp3')
+    except Exception as e:
+        print(f'Nastala chyba {e}')
+
 main_label = Label(text='Konvertor')
 main_label.grid(row=0, column=1)
 
@@ -16,7 +25,7 @@ language_label.grid(row=1, column=0)
 
 language_dropdown = ttk.Combobox(
     state='readonly',
-    values=('cs','en'),
+    values=('cs','en', 'de'),
     width=27
 )
 language_dropdown.grid(row=1, column=1)
@@ -31,14 +40,10 @@ audio_label.grid(row=3, column=0)
 audio_entry = Entry(width=30)
 audio_entry.grid(row=3, column=1)
 
-translate_button = Button(text='Přelož')
+translate_button = Button(text='Přelož', command=lambda: translate_text(text_entry.get(), language_dropdown.get(), audio_entry.get()))
 translate_button.grid(row=4, column=1)
 
 
 
 root.mainloop()
 
-text_to_audio = open('demo.txt', 'r', encoding='utf-8').read()
-output = gTTS(text=text_to_audio, lang='cs',  slow=False)
-output.save('audio.mp3')
-os.system('start audio.mp3')
